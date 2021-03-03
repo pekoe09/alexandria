06babe4530ee97422b061325b7884e11c325d963
pekoe09 <juha.kangas@welho.com>
2021-03-03 15:59:29 +0200

    Fixed wrong path in env copy.

--- scripts/InstallModules.sh
+++ scripts/InstallModules.sh
@@ -3,4 +3,4 @@ sudo rm package-lock.json
 sudo rm -r node_modules
 sudo npm install --only=production
 
-cp -f /var/nodejs/envs/alexandria/.env /var/nodejs/alexandria
+cp -f /var/envs/alexandria/.env /var/nodejs/alexandria
