f7bc14fa98fdf37039d73439abb2daed8e1d67ff
pekoe09 <juha.kangas@welho.com>
2021-03-03 15:40:51 +0200

    Removed npm install for client, once again.

--- scripts/InstallModules.sh
+++ scripts/InstallModules.sh
@@ -2,8 +2,3 @@ cd /var/nodejs/alexandria
 sudo rm package-lock.json
 sudo rm -r node_modules
 sudo npm install --only=production
-
-cd /var/nodejs/alexandria/alexandria
-sudo rm package-lock.json
-sudo rm -r node_modules
-sudo npm install --only=production
